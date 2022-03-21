66df0e7eaa347c8196a04305489d75ea8c6cd4d1
1234 <1234>
2022-03-21 23:12:11 +0300

    v 1.1.0 Отвечает на привет

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from tokens import main_token
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime
+from flask import Flask, render_template, redirect, request, make_response, url_for, abort, jsonify
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from flask.sessions import *
+from flask_restful import reqparse, abort, Api, Resource
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+# from data import db_session, news_api, news_resources, users_resource, jobs_resource
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+vk_session = vk_api.VkApi(token=main_token)
+session_api = vk_session.get_api()
+longpool = VkLongPoll(vk_session)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def sender(text, id):
+    vk_session.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0})
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+for event in longpool.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            msg = event.text
+            id = event.user_id
+            if msg.lower() == 'привет':
+                sender(id=id, text='И тебе привет!')
+
+            print(event)
